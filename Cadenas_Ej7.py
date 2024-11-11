1d3d53e1023c313f8e4a00fbbfe438f1f5580bf3
Sebiche252004 <svelasqpum@educacion.navarra.es>
2024-11-11 19:08:36 +0100

    Create Cadenas_Ej7.py

---
+++ Cadenas_Ej7.py
@@ -0,0 +1,9 @@
+
+# correo electronico
+
+correo_electronico= input("INtroduzca un correo electronico")
+division= correo_electronico.split("@")
+
+correo_final= division[0]+"@ceu.es" 
+
+print(correo_final)
