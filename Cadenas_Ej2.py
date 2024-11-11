284d407f90de5fc4b3cacbd8675295b08d86c925
Sebiche252004 <svelasqpum@educacion.navarra.es>
2024-11-11 19:03:55 +0100

    Create Cadenas_Ej2.py

---
+++ Cadenas_Ej2.py
@@ -0,0 +1,12 @@
+
+# nombre tres veces
+
+nombre= input("Introduce tu nombre completo")
+
+nombre_minusculas= nombre.lower()
+nombre_mayusculas= nombre.upper()
+nombre_titulo= nombre.title()
+
+print(nombre_minusculas, nombre_mayusculas, nombre_titulo)
+
+
