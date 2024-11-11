4ae708664e812d248a62e6e314c2815baa19a8c5
Sebiche252004 <svelasqpum@educacion.navarra.es>
2024-11-11 19:07:06 +0100

    Create Cadenas_Ej6.py

---
+++ Cadenas_Ej6.py
@@ -0,0 +1,10 @@
+
+# NOmbrE
+
+nombre= input("Introduzca su nombre")
+vocal= input("Introduzca una vocal")
+vocal_mayuscula= vocal.upper()
+
+nuevo_nombre = nombre.replace(vocal,vocal_mayuscula)
+
+print(nuevo_nombre)
