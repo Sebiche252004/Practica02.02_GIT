725a8f54cdd496344650840c374e61a2367df78a
Sebiche252004 <svelasqpum@educacion.navarra.es>
2024-11-11 19:03:17 +0100

    Create Cadenas_Ej1.py

---
+++ Cadenas_Ej1.py
@@ -0,0 +1,12 @@
+
+#online
+
+cuenta = 0
+
+
+nombre=input("Introduce tu nombre:")
+numero=int(input("Introduce elnumero de veces que lo repetire:"))
+while(cuenta<numero):
+    print(nombre)
+    print("\n")
+    cuenta=cuenta+1 
